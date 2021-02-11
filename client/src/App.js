10506eb0d824d3542213102bcc75a081b9ff99f7
Michelle Chen <mchen0062@gmail.com>
2021-02-11 12:33:45 -0800

    set up monolith

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import {useEffect, useState} from 'react'
+
+function url(path){
+  return process.env.NODE_ENV === "development" ?
+  `http://localhost:1234${path}` : path
+}
+
+function App() {
+ 
+  const [text, setText] = useState('Somewhere!')
+  useEffect(()=> {
+    // setData(text+'!')
+    fetch(url('/api/'))
+    .then(res => res.json()) //grabs API data here, which is in index at app.use
+    .then(apiText => {setText(apiText.data)}) //corresponse to key in app.use line 17
+
+  },[]) // rerun only if data changes, when you put data in the brackets. Otherwise, it'll update on first render
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Where is the API? 
+        <h1>{text}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv').config()
+const express = require("express")
+const cors = require('cors')
+const path = require("path") //another package that allows us to combine strings
+
+const app = express()
+const port = process.env.PORT || 9000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname,"client/build","index.html"))) // tells express to run the static file in client stuff 
+//when first loads
+
+
+//API is only serving information. You call onto an API. Don't need express to use json here
+app.use("/api/*",(_,res)=> {
+    res.json({data:"The API is HERE!"})
+})
+
+app.listen(port , ()=> {
+    console.log(`Server is alive on port ${port}`)
+})
+
+//CORS: cross origin resource sharing - will add in specific headers to 
+//allow passage 
+
+// console.log("Hello there")
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(process.env.USER)
+// console.log(process.env.PORT) 
+// console.log(dotenv)
+// console.log(dotenv.PORT)
+// console.log(proccess.env.FOOD)
+
