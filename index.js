63547071e531c1a8a6fe15c403a33b37a9c9eb9c
Michelle Chen <mchen0062@gmail.com>
2021-02-11 12:48:09 -0800

    debug

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const dotenv = require('dotenv').config()
 const express = require("express")
 const cors = require('cors')
 const path = require("path") //another package that allows us to combine strings
+const { dirname } = require('path')
 
 const app = express()
 const port = process.env.PORT || 9000
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname,"client/build","index.html"))) // tells express to run the static file in client stuff 
+app.use(express.static(path.join(__dirname,"client/build"))) // tells express to run the static file in client stuff 
 //when first loads
 
 
 //API is only serving information. You call onto an API. Don't need express to use json here
 app.use("/api/*",(_,res)=> {
     res.json({data:"The API is HERE!"})
+})
+
+app.use("*",(_,res)=> {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 app.listen(port , ()=> {
